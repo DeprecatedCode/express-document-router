2b7f338bea06abba1ac78b73a0dfbc15447c03ce
Robbie Trencheny <me@robbiet.us>
2012-04-30 11:39:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,35 @@
  * Express Document Router
  * @author Nate Ferrero
  */
+var findit = require('findit');
+var path = require('path');
+var fs = require('fs');
 module.exports.route = function(app, dir, types) {
 
-	/**
-	 * Iterate Through Directories
-	 * @author Robbie Trencheny
-	 */
+  /**
+   * Iterate Through Directories
+   * @author Robbie Trencheny
+   */
 
-
-	for each one check types[extension] such as type.jade
-	do app.get('/path/to/file', function(req, res) {
-
-		load file contents
-
-		then
-
-		types[extension](file contents, function(html) {
-			res.send(html);
-		});
-	})
-
+  findit.find(dir, function (file) {
+    // Split extension
+    var ext = path.extname(file).replace('.', '');
+    // Split route
+    var route = file.replace(dir, '').replace('.'+ext, '').replace(/\/index$/, '');
+    // Match ext type
+    if(types[ext]) {
+      // Define the route
+      app.get(route, function(req, res) {
+        // Read file contents
+        fs.readFile(file, 'utf8', function (err, data) {
+          if (err) throw err;
+          // Hollaback to handler
+          types[ext](data, function(html){
+            // Render the content
+            res.send(html);
+          });
+        });
+      });
+    }
+  });
 }
